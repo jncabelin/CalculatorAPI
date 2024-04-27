a74f6a9546477be28360071d08392358c8eef549
diethermbp <diethercabelin@gmail.com>
2024-04-27 17:53:53 +0800

    updated application project

---
+++ CalculatorApp.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculatorApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            return services;
+        }
+    }
+}
+
