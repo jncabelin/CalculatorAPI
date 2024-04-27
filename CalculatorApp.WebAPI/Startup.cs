2f5e6b7c081db00992146a8da279f301f5823018
diethermbp <diethercabelin@gmail.com>
2024-04-27 17:53:08 +0800

    updated webapi project

---
+++ CalculatorApp.WebAPI/Startup.cs
@@ -0,0 +1,57 @@
+﻿using CalculatorApp.Api.Services.Interfaces;
+using CalculatorApp.Application;
+using CalculatorApp.Application.Services;
+using Microsoft.OpenApi.Models;
+
+namespace CalculatorApp.API
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication();
+            services.AddControllers();
+            services.AddSingleton<ICalculatorService, CalculatorService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Calculator API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculator API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
